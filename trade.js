b106bf0f0c3577e2ebc962a42c05aca0e3e55922
md_dev <m.damian9@hotmail.com>
2018-07-16 01:19:11 -0700

    Completed 'newExitTrade()' function, adjusted functions, adjusted comments, did some testing, functions work fine

--- trade.js
+++ trade.js
@@ -5,8 +5,8 @@ var fs = require("fs");
 // "newEntryTrade()" function
 // This function runs when the user wants to create a new entry (buy) trade. By using inquirer, the user is prompted for their initial 
 // investment, the price of Bitcoin when they bought, the name of the altcoin they bought, and the price of the altcoin they bought 
-// (altcoin price is in BTC). This function assumes you are buying BTC on Coinbase with a 4% purchase fee and a 0.000014 BTC transfer fee 
-// to altcoin exchange.
+// (altcoin price is in BTC). This function assumes the user is buying BTC on Coinbase with a 4% purchase fee, a 0.000014 BTC transfer fee 
+// to altcoin exchange, and a 0.1% purchase fee at the altcoin exchange when buying an altcoin.
 function newEntryTrade() {
     inquirer.prompt([
         {
@@ -17,7 +17,7 @@ function newEntryTrade() {
         {
             type: "input",
             name: "btcPrice",
-            message: "Enter Bitcoin price: "
+            message: "Enter Bitcoin price (bought): "
         },
         {
             type: "input",
@@ -34,22 +34,25 @@ function newEntryTrade() {
         var actualBTC = totalBTC - (totalBTC * .04);
         var transferredBTC = actualBTC - 0.000014;
         var totalCoins = transferredBTC / parseFloat(response.altPrice);
-        var entryPrice = parseFloat(response.investment) / totalCoins;
+        var exchFee = totalCoins * .001;
+        var actualCoins = totalCoins - exchFee;
+        var entryPrice = parseFloat(response.investment) / actualCoins;
         var output = "* New entry *\nCryptocurrency: " + response.altName + "\nInitial investment: $" + response.investment +
-            "\nBTC Price bought at: $" + response.btcPrice + " per BTC\nBought " + response.altName + " at: " + response.altPrice +
-            " BTC\nTotal coins bought: " + totalCoins + " " + response.altName + "\nEntry price: $" + entryPrice +
+            "\nBought Bitcoin at: $" + response.btcPrice + " per BTC\nBought " + response.altName + " at: " + response.altPrice +
+            " BTC\nTotal coins bought: " + actualCoins + " " + response.altName + "\nEntry price: $" + entryPrice +
             " (factoring in Coinbase fee & transfer fees)\n";
         console.log(output);
-        fs.appendFile('./log.txt', output + "\n", function (error) {
+        fs.appendFile('./entries.txt', output + "\n", function (error) {
             if (error) throw error;
         });
     });
 };
 
-// This function is incomplete
 // "newExitTrade()" function
 // This function runs when the user wants to create a new exit (sell) trade. By using inquirer, the user is prompted for the name of the 
-// altcoin they sold, the price they sold the altcoin at (altcoin price is in BTC), [comment needs to be completed]
+// altcoin they sold, the amount of conis / tokens they sold, the price they sold the altcoin at (altcoin price is in BTC), then the price
+// they sold Bitcoin at in the end. This function assumes the user is selling altcoin on altcoin exchange with a 0.1% trade fee, charged a 
+// 0.000014 BTC transfer fee to Coinbase, and a 4% trade fee at Coinbase when selling Bitcoin.
 function newExitTrade() {
     inquirer.prompt([
         {
@@ -59,13 +62,34 @@ function newExitTrade() {
         },
         {
             type: "input",
+            name: "numCoinsSold",
+            message: "Enter amount of coins / tokens sold: "
+        },
+        {
+            type: "input",
             name: "altPrice",
             message: "Enter price altcoin was sold at (in BTC): "
+        },
+        {
+            type: "input",
+            name: "btcPrice",
+            message: "Enter Bitcoin price (sold): "
         }
-        // User still needs to input other parameters to complete function (coins sold, and btcPrice sold)
     ]).then(function (response) {
-        var output = "* New exit *\nCryptocurrency: " + response.altName + "\nPrice sold at (in BTC): " + response.altPrice + "\n";
+        var totalBTC = response.numCoinsSold * response.altPrice;
+        var exchFee = totalBTC * .001;
+        var actualBTC = totalBTC - exchFee;
+        var transferredBTC = actualBTC - .000014;
+        var divestment = transferredBTC * response.btcPrice;
+        var actualDivestment = divestment - (divestment * .04);
+        var exitPrice = actualDivestment / response.numCoinsSold;
+        var output = "* New exit *\nCryptocurrency: " + response.altName + "\nAmount of coins / tokens sold: " + response.numCoinsSold +
+            " " + response.altName + "\nSold " + response.altName + " at: " + response.altPrice + " BTC\nSold Bitcoin at: $" + 
+            response.btcPrice + " per BTC\nTotal Bitcoin sold: " + transferredBTC + " BTC\nExit price: $" + exitPrice + "\n";
         console.log(output);
+        fs.appendFile('./exits.txt', output + "\n", function (error) {
+            if (error) throw error;
+        });
     });
 };
 
