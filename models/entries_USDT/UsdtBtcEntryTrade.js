82ae243a8133cabb788f0c0044810916ea392198
mdamian9 <m.damian9@hotmail.com>
2018-10-01 23:38:43 -0700

    Organized models into folders, created UsdtBtcEntryTrade.js -> new schema for USDT / BTC entry trade object

---
+++ models/entries_USDT/UsdtBtcEntryTrade.js
@@ -0,0 +1,55 @@
+// USDT -> BTC entry trade model
+
+// Require "mongoose"
+const mongoose = require("mongoose");
+
+// Schema class created with mongoose schema method
+const Schema = mongoose.Schema;
+
+// Trade schema with UsdtBtcEntryTrade object
+const tradeSchema = new Schema({
+    cryptocurrency: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    initialInvestment: {
+        type: Number,
+        required: true
+    },
+    btcPriceBought: {
+        type: Number,
+        required: true
+    },
+    totalBTC: {
+        type: Number,
+        required: true
+    },
+    altPrice: {
+        type: Number,
+        required: true
+    },
+    totalAlt: {
+        type: Number,
+        required: true
+    },
+    entryPriceUSDT: {
+        type: Number,
+        required: true
+    },
+    entryPriceBTC: {
+        type: Number,
+        required: true
+    },
+    dateLogged: {
+        type: String,
+        trim: true,
+        required: true
+    }
+});
+
+// Create the USDT -> BTC entry trade model using the trade schema
+const UsdtBtcEntryTrade = mongoose.model("UsdBtcEntryTrade", tradeSchema);
+
+// Export the USDT -> BTC entry trade model
+module.exports = UsdtBtcEntryTrade;
--- models/index.js
+++ models/index.js
@@ -1,3 +1,3 @@
 module.exports = {
-    UsdBtcEntryTrade: require("./UsdBtcEntryTrade")
+    UsdBtcEntryTrade: require("./entries_USD/UsdBtcEntryTrade")
 };
