3233df66be244ee944b18211482b3ca2cf5213f3
mdamian9 <m.damian9@hotmail.com>
2018-10-02 00:02:29 -0700

    Updated index.js in models folder

--- models/index.js
+++ models/index.js
@@ -1,3 +1,11 @@
 module.exports = {
-    UsdBtcEntryTrade: require("./entries_USD/UsdBtcEntryTrade")
+    UsdBtcEntryTrade: require("./entries_USD/UsdBtcEntryTrade"),
+    UsdEthEntryTrade: require("./entries_USD/UsdEthEntryTrade"),
+    UsdEntryTrade: require("./entries_USD/UsdEntryTrade"),
+    UsdtBtcEntryTrade: require("./entries_USDT/UsdtBtcEntryTrade"),
+    UsdtEthEntryTrade: require("./entries_USDT/UsdtEthEntryTrade"),
+    UsdtEntryTrade: require("./entries_USDT/UsdtEntryTrade"),
+    BtcEntryTrade: require("./entries_CRYPTO/BtcEntryTrade"),
+    EthEntryTrade: require("./entries_CRYPTO/EthEntryTrade"),
+    BnbEntryTrade: require("./entries_CRYPTO/BnbEntryTrade")
 };
